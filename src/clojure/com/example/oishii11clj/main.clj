6ef3cf3b0588bfe3e13d889c16c95e040f84ae7f
ka <ka.kaosf@gmail.com>
2013-11-30 14:20:13 +0900

    initialize
    execute a following command;
      lein droid new oishii11clj com.example.oishii11clj \
        :activity MainActivity \
        :target-sdk 17 \
        :app-name oishii-11-clj

---
+++ src/clojure/com/example/oishii11clj/main.clj
@@ -0,0 +1,13 @@
+(ns com.example.oishii11clj.main
+  (:use [neko.activity :only [defactivity set-content-view!]]
+        [neko.threading :only [on-ui]]
+        [neko.ui :only [make-ui]]))
+
+(defactivity com.example.oishii11clj.MainActivity
+  :def a
+  :on-create
+  (fn [this bundle]
+    (on-ui
+     (set-content-view! a
+      (make-ui [:linear-layout {}
+                [:text-view {:text "Hello from Clojure!"}]])))))
